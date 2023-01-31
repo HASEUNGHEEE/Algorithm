ac4899bcd9195f31f22c29e1f478600366b77c03
HASEUNGHEE <ariul123@gmail.com>
2023-01-31 10:45:34 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.java
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
+        sc.close();
         
         while (str.length() > 10) {
             System.out.println(str.substring(0, 10));
