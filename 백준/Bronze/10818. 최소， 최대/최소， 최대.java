e611d57cd0778f5e31c22dab1e4bd4f8c3c9d8fd
HASEUNGHEE <ariul123@gmail.com>
2023-02-01 10:47:25 +0900

    [Bronze III] Title: 최소, 최대, Time: 572 ms, Memory: 89952 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,15 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Integer[] numbers = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
-        Arrays.sort(numbers);
-        System.out.println(numbers[0] + " " + numbers[n-1]);
+        int max = -1000001;
+        int min = 1000001;
+        
+        while (st.hasMoreTokens()) {
+            int value = Integer.parseInt(st.nextToken());
+            if (value > max) {
+                max = value;
+            }
+            if (value < min) {
+                min = value;
+            }
+        }
+        System.out.println(min + " " + max);
     }
 }
