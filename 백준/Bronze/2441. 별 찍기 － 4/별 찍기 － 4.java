d9868303d73fd6bcab8ed60dd1afbbd6e75f49e8
HASEUNGHEE <ariul123@gmail.com>
2023-02-03 13:49:45 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < n-i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
