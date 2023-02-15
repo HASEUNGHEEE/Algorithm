4fdda530c57cc692acff4f6ce50af7b2633dde54
HASEUNGHEE <ariul123@gmail.com>
2023-02-15 20:46:15 +0900

    [Bronze V] Title: 팩토리얼, Time: 132 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(factorial(N));
+    }
+    
+    public static int factorial(int N) {
+        if (N <= 1) return 1; // 재귀 종료 조건
+        return N * factorial(N - 1);
+    }
+}
