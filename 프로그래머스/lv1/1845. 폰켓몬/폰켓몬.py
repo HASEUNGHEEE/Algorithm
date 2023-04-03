37de56a9e8ec2c329071fe545e93d994314ca059
HASEUNGHEE <ariul123@gmail.com>
2023-04-04 08:46:43 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,10 @@
 def solution(nums):
+    return min(len(nums)/2, len(set(nums)))
+    
+    '''
     unique_nums = len(set(nums))
     if len(nums) / 2 > unique_nums:
         return unique_nums
     else:
         return len(nums) / 2
+    '''
