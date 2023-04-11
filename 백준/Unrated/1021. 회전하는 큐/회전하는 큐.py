597fe54976944f573c519111370fa863e3d0ff4f
HASEUNGHEE <ariul123@gmail.com>
2023-04-12 08:58:36 +0900

    [Unrated] Title: 회전하는 큐, Time: 64 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Unrated/1021. 회전하는 큐/회전하는 큐.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+position = list(map(int, input().split()))
+
+# deque([1, 2, 3, ... , n])
+deque = deque([i for i in range(1, n+1)])
+
+cnt = 0
+# 뽑아내려는 위치
+for i in position:
+    while True:
+        if deque[0] == i: # deque의 첫번째 인덱스가 뽑아내려는 위치와 같을 때
+            deque.popleft()
+            break
+        else:
+            if deque.index(i) <= len(deque) // 2: 
+                while deque[0] != i: # deque의 첫번째 인덱스와 뽑을 위치 i가 같아질 때까지
+                    deque.append(deque.popleft())
+                    cnt += 1
+            else:
+                while deque[0] != i:
+                    deque.appendleft(deque.pop())
+                    cnt += 1
+print(cnt)            
+            
