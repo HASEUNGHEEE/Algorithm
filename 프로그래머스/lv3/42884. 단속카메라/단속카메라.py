7e52a766ca245e0f44433b1e46f309e10d14c79f
HASEUNGHEE <ariul123@gmail.com>
2023-04-14 19:17:00 +0900

    [level 3] Title: 단속카메라, 직관적인 이해를 위해 변수명 변경

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,6 +1,5 @@
 def solution(routes):
-    answer = 0
-    entry_point = -30001
+    last_camera = -30001
     camera_cnt = 0
     
     # 진출 지점 기준으로 2차원 리스트 오름차순 정렬
@@ -8,8 +7,8 @@ def solution(routes):
     
     # 2차원 리스트 반복하며 값(1차원 리스트)에 접근
     for route in routes:
-        if route[0] > entry_point:
+        if route[0] > last_camera:
             camera_cnt += 1
-            entry_point = route[1]
+            last_camera = route[1]
     
     return camera_cnt
