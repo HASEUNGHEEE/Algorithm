c075f56fe5db9db002e2c6470800635320d1e10c
HASEUNGHEE <ariul123@gmail.com>
2023-05-15 14:37:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31332 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-data = [list(input()) for _ in range(n)]
-
 # 상하좌우
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-cnt=0
-apt=[]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def dfs(x,y):
-    global cnt
-    data[x][y] = '0'
-    cnt+=1
-    for i in range(4):
-        nx = x + dx[i] 
-        ny = y + dy[i]
-        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-            continue
-        if data[nx][ny] == '1':
-            dfs(nx,ny)
+# dfs
+def dfs(x, y):
+  global cnt
+  graph[x][y] = 0
+  cnt += 1
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
+      dfs(nx, ny)
 
+# 입력
+n = int(input())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
+# 단지내 집의 수
+apt = []
 for i in range(n):
-    for j in range(n):
-        if data[i][j] == '1':
-            cnt= 0
-            dfs(i,j)
-            apt.append(cnt)
-
+  for j in range(n):
+    if graph[i][j] == 1:
+      cnt = 0
+      dfs(i, j)
+      apt.append(cnt)
+  
+# 출력
 apt.sort()
-print(len(apt), *apt, sep = '\n')
+print(len(apt))
+for x in apt:
+  print(x)
