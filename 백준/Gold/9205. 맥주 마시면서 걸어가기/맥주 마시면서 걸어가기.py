b741de35d57858bf971fba32fd47f9026b5ad3ba
HASEUNGHEE <ariul123@gmail.com>
2023-05-24 20:38:09 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 68 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs():
+    q = deque()
+    q.append([start[0], start[1]])
+    while q:
+        x, y = q.popleft()
+        if abs(x - end[0]) + abs(y - end[1]) <= 1000:
+            print("happy")
+            return
+        for i in range(n):
+            if not visited[i]:
+                nx, ny = location[i]
+                if abs(x - nx) + abs(y - ny) <= 1000:
+                    q.append([nx, ny])
+                    visited[i] = 1
+    print("sad")
+    return
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    start = [int(x) for x in input().split()]
+    location = []
+    for j in range(n):
+        x, y = map(int, input().split())
+        location.append([x, y])
+    end = [int(x) for x in input().split()]
+    visited = [0 for i in range(n+1)] #home 제외
+    bfs()
