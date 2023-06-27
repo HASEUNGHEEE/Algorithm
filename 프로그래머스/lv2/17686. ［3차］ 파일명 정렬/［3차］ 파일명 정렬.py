8e3bf004a91844e60ef84cf64484b068336bcaf0
HASEUNGHEE <ariul123@gmail.com>
2023-06-28 08:55:00 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.03 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,21 +1,3 @@
 def solution(files):
     answer = []
-    
-    for file in files:
-        # head, number, tail 분리
-        head, number, tail = '', '', ''
-        for i in range(len(file)):
-            if not file[i].isdigit() and not number:
-                head += file[i]
-            elif file[i].isdigit() and head:
-                number += file[i]
-            else:
-                tail += file[i:]
-                break
-        answer.append((head, number, tail))
-        
-    # 정렬
-    answer.sort(key = lambda x: [x[0].lower(), int(x[1])])
-    
-    
-    return [''.join(i) for i in answer]
+    return answer
