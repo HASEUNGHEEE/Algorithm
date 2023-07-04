f74e0029339630308e9c3b053f3d01d25fe99d6b
HASEUNGHEE <ariul123@gmail.com>
2023-07-05 08:26:03 +0900

    [level 1] Title: 최소직사각형, Time: 3.40 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,4 +1,5 @@
 def solution(sizes):
+    '''
     large_num = [] # 60, 70, 60, 80
     small_num = [] # 50, 30, 30, 40
     
@@ -10,3 +11,6 @@ def solution(sizes):
     small_num.sort(reverse=True)
         
     return large_num[0] * small_num[0]
+    '''
+
+    return max(max(size) for size in sizes) * max(min(size) for size in sizes)
