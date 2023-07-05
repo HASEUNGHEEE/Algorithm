f89885652895cc7ac7f17c2c88fd0b7b7b30a17d
HASEUNGHEE <ariul123@gmail.com>
2023-07-06 08:41:33 +0900

    [level 1] Title: 모의고사, Time: 3.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,21 +1,3 @@
 def solution(answers):
-    pattern_1 = [1, 2, 3, 4, 5]
-    pattern_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    pattern_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    scores = [0, 0, 0]
-    ans = []
-    
-    for idx, answer in enumerate(answers):
-        # tc1 : (0, 1) (1, 2) (2, 3) (3, 4) (4, 5)
-        if answer == pattern_1[idx % len(pattern_1)]:
-            scores[0] += 1
-        if answer == pattern_2[idx % len(pattern_2)]:
-            scores[1] += 1
-        if answer == pattern_3[idx % len(pattern_3)]:
-            scores[2] += 1
-    
-    for idx, score in enumerate(scores):
-        if score == max(scores):
-            ans.append(idx+1)
-            
-    return ans
+    answer = []
+    return answer
