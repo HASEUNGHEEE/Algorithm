f112acebc1b9856b8854bcd74eaf73c418c10dae
HASEUNGHEE <ariul123@gmail.com>
2023-07-07 09:43:44 +0900

    [level 2] Title: 소수 찾기, Time: 5.29 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,5 +1,6 @@
 import math
 from itertools import permutations
+"""
 # 소수 판별 함수
 def is_prime_number(x):
     if x < 2:
@@ -21,3 +22,22 @@ def solution(numbers):
                 answer.append(num)
     
     return len(set(answer))
+
+"""
+
+# 같은 로직, 더욱 간결한 코드
+def solution(numbers):
+    num_set = set()
+    
+    for i in range(len(numbers)):
+        # 1. list(n)의 원소들 중에서 i+1 개의 원소를 뽑아 순열을 만들고
+        # 2. join하여 int화
+        # 3. set에 담아 num_set에 합친다(|= 병합 업데이트 연산자)
+        num_set |= set(map(int, map("".join, permutations(list(numbers), i+1))))
+    
+    num_set -= set(range(0,2)) # 0과 1은 set에서 제거
+    
+    for i in range(2, int(max(num_set) ** 0.5) + 1): # 2부터 set 최댓값의 제곱근까지의 모든 수를 확인하면서
+        num_set -= set(range(i * 2, max(num_set) + 1, i)) # set에서 배수들을 다 제거한다
+        
+    return len(num_set)
