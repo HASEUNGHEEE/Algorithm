abe68f79db28ad9241f89f0ea75507e563750b30
HASEUNGHEE <ariul123@gmail.com>
2023-09-07 11:31:27 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,24 @@
+import heapq as hq
+def solution(jobs):
+    answer, time, cnt = 0, 0, 0
+    length = len(jobs)
+    heap = []
+    jobs.sort()
+    
+    while cnt < length:
+        while len(jobs) > 0:
+            if jobs[0][0] <= time:
+                hq.heappush(heap, jobs.pop(0)[::-1]) # [소요시간, 요청시점] 순으로 힙에 push
+            else:
+                break
+        if len(heap) > 0:
+            job = hq.heappop(heap)
+            time += job[0] # 소요시간
+            answer += time - job[1]
+            cnt += 1
+        else:
+            time += 1
+    
+    return answer // length # 소수점 이하의 수 버림
+
+
